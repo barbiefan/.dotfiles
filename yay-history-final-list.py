5f2f96a00a0ba7b29bc5c3441f1772b1138026e7
Max Tumanov <georgecarlin770@gmail.com>
2021-08-18 10:23:12 +0300

    ---

--- yay-history-final-list.py
+++ yay-history-final-list.py
@@ -11,14 +11,14 @@ installed_parsed = []
 for i, entry in enumerate(installed):
     packages = entry[entry.find('-S ')+3:].split(' ')
     for i in range(len(packages)):
-        packages[i] = re.sub('[\W_]+', '', packages[i])
+        packages[i] = re.sub('[^0-9a-zA-z -]', '', packages[i])
     installed_parsed += packages
 
 uninstalled_parsed = []
 for i, entry in enumerate(uninstalled):
     packages = entry[entry.find('-R ')+3:].split(' ')
     for i in range(len(packages)):
-        packages[i] = re.sub('[\W_]+', '', packages[i])
+        packages[i] = re.sub('[^0-9a-zA-z -]', '', packages[i])
     uninstalled_parsed += packages
 
 packages_left = list(set(installed_parsed)-set(uninstalled_parsed))
