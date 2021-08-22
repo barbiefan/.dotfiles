a1ea8fc0721be7232c23fab2119a70efb1bdcf57
Max Tumanov <georgecarlin770@gmail.com>
2021-08-22 23:36:19 +0300

    sublime config folder

---
+++ .config/sublime-text/Packages/User/python.py
@@ -0,0 +1,14 @@
+import math
+
+a = "suck fuck and suck"
+
+print(f"suck on these {math.pi}")
+
+def suck(what):
+    # what: what to suck
+    print(f'sucking {what}')
+
+
+def my_suck_and_fuck():
+    for fuck in a:
+        suck(fuck)
